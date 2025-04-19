7c3ac9f503fc89422110f4bb8af37015a71f8009
sergiofigueroa22 <serfigue22@gmail.com>
2025-04-19 12:32:58 -0500

    comentarios

--- src/main/java/safide/erp/inventory/domain/model/InveProdCata.java
+++ src/main/java/safide/erp/inventory/domain/model/InveProdCata.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class InveProdCata {
 	private Integer id;
-	private Integer id_macohe;
+	private Integer id_macohe;//id company g
     private Integer id_macode;
 	private String name_inprca;
 	private String alterna_inprca;
