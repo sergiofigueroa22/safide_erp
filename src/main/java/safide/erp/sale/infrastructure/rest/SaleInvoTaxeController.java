017b2182842162fe16519bf0e2c9176a9961f45d
sergiofigueroa22 <serfigue22@gmail.com>
2025-04-27 13:26:41 -0500

    class taxes invoice

--- src/main/java/safide/erp/sale/infrastructure/rest/SaleInvoTaxeController.java
+++ src/main/java/safide/erp/sale/infrastructure/rest/SaleInvoTaxeController.java
@@ -1,9 +1,7 @@
 package safide.erp.sale.infrastructure.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import safide.erp.generic.application.port.GeneErpService;
 import safide.erp.generic.infrastructure.rest.GeneErpController;
 import safide.erp.sale.application.port.SaleInvoTaxeService;
 import safide.erp.sale.domain.model.SaleInvoTaxe;
